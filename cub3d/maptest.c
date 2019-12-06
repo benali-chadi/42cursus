a6de86965ed8d7cc41e55ef4e511f32f0012d97a
Chadi Benali oulhaj <cbenali-@e2r3p1.1337.ma>
2019-12-06 21:36:56 +0100

    cub3d

--- cub3d/maptest.c
+++ cub3d/maptest.c
@@ -7,7 +7,7 @@ void    *mlx_ptr;
 void    *win_ptr;
 int     **map;
 
-void put_square(void *mlx_ptr, void *win_ptr, int x, int y, int size, int color)
+void put_square(int x, int y, int size, int color)
 {
     int d;
     int d2;
@@ -28,9 +28,8 @@ void put_square(void *mlx_ptr, void *win_ptr, int x, int y, int size, int color)
 	}
 }
 
-int **init_map(int x, int y)
+void init_map(int x, int y)
 {
-//    int **map;
     int i;
     int j;
 
@@ -45,15 +44,14 @@ int **init_map(int x, int y)
             if (i == 0 || i == y - 1 || j == 0 || j == x - 1)
                 map[i][j] = 1;
             else
-                map[i][j] = 0; 
+                map[i][j] = 0;
             j++;
         }
         i++;
     }
-    return (map);
 }
 
-void draw_map(/*int **map, */int x, int y)
+void draw_map(int x, int y)
 {
     int i;
     int j;
@@ -62,13 +60,13 @@ void draw_map(/*int **map, */int x, int y)
     i = 0;
     j = 0;
     size = 50;
-    while (i < 12)
+    while (i < 10)
     {
         j = 0;
-        while(j < 12)
+        while(j < 10)
         {
             if(map[i][j] == 1)
-                put_square(mlx_ptr, win_ptr, x, y, size, 0xffdddd);
+                put_square(x, y, size, 0x55aadd);
             x += size;
             j++;
         }
@@ -82,57 +80,53 @@ int move(int key, int *cord)
 {
     int x = cord[0];
     int y = cord[1];
-    if (key == 126)
+    int color = 0x55aadd;
+
+    if (key == 126 && y > 50)
     {
-        mlx_clear_window(mlx_ptr, win_ptr);
-        draw_map(0 , 0);
-        put_square(mlx_ptr, win_ptr, x, y - 20, 60, 0xffdddd);
+        put_square(x, y - 20, 60, color);
         *(cord + 1) -= 20;
-    }
-    if (key == 125)
+    }  
+    else if (key == 125 && y < 390)
     {
-        mlx_clear_window(mlx_ptr, win_ptr);
-        draw_map(0 , 0);
-        put_square(mlx_ptr, win_ptr, x, y + 20, 60, 0xffdddd);
+        put_square(x, y + 20, 60, color);
         *(cord + 1) += 20;
     }
-    if (key == 123)
+    else if (key == 123 && x > 50)
     {
-        mlx_clear_window(mlx_ptr, win_ptr);
-        draw_map(0 , 0);
-        put_square(mlx_ptr, win_ptr, x - 20, y, 60, 0xffdddd);
+        put_square(x - 20, y, 60, color);
         *(cord) -= 20;
     }
-    if (key == 124)
+    else if (key == 124 && x < 390)
     {
-        mlx_clear_window(mlx_ptr, win_ptr);
-        draw_map(0 , 0);
-        put_square(mlx_ptr, win_ptr, x + 20, y, 60, 0xffdddd);
+        put_square(x + 20, y, 60, color);
         *(cord) += 20;
     }
+    else
+        put_square(x, y, 60, color);
+    mlx_clear_window(mlx_ptr, win_ptr);
+    draw_map(0 , 0);
     return (0);
 }
 
 int main()
 {
-//    int **map = 
     int x;
     int y;   
 
     mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 600, 600, "mlx 1337");
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 1337");
     x = 0;
-    y = 0;
-    // drawing map
-    
-    init_map(12, 12);
+    y = 0;   
+    init_map(10, 10);
     draw_map(x, y);
-    x = 450;
-    y = 450;
-    put_square(mlx_ptr, win_ptr, x, y, 60, 0xffdddd);
+    x = 250;
+    y = 250;
+    put_square(x, y, 60, 0x55aadd);
     int cord[2];
     cord[0] = x;
     cord[1] = y;
     mlx_key_hook(win_ptr, move, cord);
 	mlx_loop(mlx_ptr);
+    return(0);
 }
