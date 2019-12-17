029b0e6ce96015d08041463a898806600310d79b
Chadi Benali oulhaj <cbenali-@e1r2p4.1337.ma>
2019-12-17 22:02:31 +0100

    t

--- cub3d/maptest.c
+++ cub3d/maptest.c
@@ -1,132 +1,181 @@
-#include <mlx.h>
-#include <stdlib.h>
-#include "libft/libft.h"
-#include <unistd.h>
+#include "cube3d.h"
+
+# define INCREMENT_Y cos((player->phi_start + 30) * VAL) * 5
+# define INCREMENT_X sin((player->phi_start + 30) * VAL) * 5;
 
 void    *mlx_ptr;
 void    *win_ptr;
-int     **map;
+int     wx;
+int     wy;
+int     count;
 
-void put_square(int x, int y, int size, int color)
+void init_pl_map(t_player *player)
+{
+    player->phi_start = 150;
+    player->phi_end = 210;
+    player->color = 0x48CFAF;
+}
+
+void put_character(t_player player)
+{
+    int r;
+	float phi;
+    int or_x;
+    int or_y;
+
+    or_x = player.x_p;
+    or_y = player.y_p;
+	r = 2;
+	phi = 0;
+	while (phi <= 360)
+	{
+		player.x_p = or_x + (r * sin(phi * VAL));
+		player.y_p = or_y + (r * cos(phi * VAL));
+		mlx_pixel_put(mlx_ptr, win_ptr, player.x_p, player.y_p, player.color);
+		phi += 0.1;
+	}
+    phi = player.phi_start;
+    while (r < 60)
+    {
+        player.phi_start = phi;
+        while (player.phi_start < player.phi_end)
+        {
+            player.x_p = or_x + (r * sin(player.phi_start * VAL));
+	    	player.y_p = or_y + (r * cos(player.phi_start * VAL));
+            mlx_pixel_put(mlx_ptr, win_ptr, player.x_p, player.y_p, player.color);
+	    	player.phi_start += 0.1;
+        }
+        r++;
+    }
+}
+
+void put_square(t_player player, int color)
 {
     int d;
     int d2;
     int or_x;
 
-    or_x = x;
-    d2 = y + size;
-    d = x + size;
-	while (y < d2)
+    or_x = player.x_m;
+    d2 = player.y_m + TILE_SIZE;
+    d = player.x_m + TILE_SIZE;
+	while (player.y_m < d2)
 	{
-        x = or_x;
-		while (x < d)
+        player.x_m = or_x;
+		while (player.x_m < d)
 		{
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
-			x++;
+			mlx_pixel_put(mlx_ptr, win_ptr, player.x_m, player.y_m, color);
+			player.x_m++;
 		}
-		y++;
+		player.y_m++;
 	}
 }
 
-void init_map(int x, int y)
+void draw_map(t_player *player)
 {
     int i;
     int j;
-
-    i = 0;
-    map = malloc(y * sizeof(int *));
-    while (i < y)
-    {
-        j = 0;
-        map[i] = malloc(x * sizeof(int));
-        while (j < x)
-        {
-            if (i == 0 || i == y - 1 || j == 0 || j == x - 1)
-                map[i][j] = 1;
-            else
-                map[i][j] = 0;
-            j++;
-        }
-        i++;
-    }
-}
-
-void draw_map(int x, int y)
-{
-    int i;
-    int j;
-    int size;
 
     i = 0;
     j = 0;
-    size = 50;
-    while (i < 10)
+    player->x_m = 0;
+    player->y_m = 0;
+    while (i < MAP_Y)
     {
         j = 0;
-        while(j < 10)
+        while(j < MAP_X)
         {
             if(map[i][j] == 1)
-                put_square(x, y, size, 0x55aadd);
-            x += size;
+                put_square(*player, 0x0351C1);
+            else if (map[i][j] == 2)
+                put_square(*player, 0x00CF91);
+            else if (map[i][j] == 3)
+                put_square(*player, 0x7367F0);
+            else if (map[i][j] == 4)
+                put_square(*player, 0xB6BCF2);
+            else if (map[i][j] == 'N' && !count)
+            {
+                player->x_p = player->x_m;
+                player->y_p = player->y_m;
+                put_character(*player);
+                count++;
+            }
+            player->x_m += TILE_SIZE;
             j++;
         }
-        x = 0;
-        y += size;
+        player->x_m = 0;
+        player->y_m += TILE_SIZE;
         i++;
     }
 }
 
-int move(int key, int *cord)
+int has_wall(t_player player, int con)
 {
-    int x = cord[0];
-    int y = cord[1];
-    int color = 0x55aadd;
+    float fory;
+    float forx;
 
-    if (key == 126 && y > 50)
+    fory = cos((player.phi_start + 30) * VAL) * 5;
+    forx = sin((player.phi_start + 30) * VAL) * 5;
+    player.y_p = (con == 1 ? player.y_p + fory : player.y_p - fory);
+    player.x_p = (con == 1 ? player.x_p + forx : player.x_p - forx);
+    return (map[(int)player.y_p / TILE_SIZE][(int)player.x_p / TILE_SIZE] != 0);
+}
+
+int move(int key, t_player *player)
+{
+    if (key == 126 && !has_wall(*player, 1))
     {
-        put_square(x, y - 20, 60, color);
-        *(cord + 1) -= 20;
+        player->y_p += INCREMENT_Y;
+        player->x_p += INCREMENT_X; 
     }  
-    else if (key == 125 && y < 390)
+    else if (key == 125 && !has_wall(*player, 2))
     {
-        put_square(x, y + 20, 60, color);
-        *(cord + 1) += 20;
+        player->y_p -= INCREMENT_Y;
+        player->x_p -= INCREMENT_X; 
     }
-    else if (key == 123 && x > 50)
+    else if (key == 124)
     {
-        put_square(x - 20, y, 60, color);
-        *(cord) -= 20;
+        player->phi_start -= 10;
+        player->phi_end -= 10;
     }
-    else if (key == 124 && x < 390)
+    else if (key == 123)
     {
-        put_square(x + 20, y, 60, color);
-        *(cord) += 20;
+        player->phi_start += 10;
+        player->phi_end += 10;
     }
-    else
-        put_square(x, y, 60, color);
+    else if (key == 53)
+    {
+        mlx_destroy_window(mlx_ptr, win_ptr);
+        exit(0);
+    }
     mlx_clear_window(mlx_ptr, win_ptr);
-    draw_map(0 , 0);
+    put_character(*player);
+    draw_map(player);
+    // printf("x = %f, y = %f\n", player->x_p, player->y_p);
     return (0);
 }
 
+// int loop(int key, t_player *player)
+// {
+//     mlx_clear_window(mlx_ptr, win_ptr);
+//     draw_map(player);
+//     put_character(*player);
+//     return(0);
+// }
+
 int main()
 {
-    int x;
-    int y;   
+    t_player player;
+    count = 0;
 
+    wx = MAP_X * TILE_SIZE;
+    wy = MAP_Y * TILE_SIZE;
     mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 1337");
-    x = 0;
-    y = 0;   
-    init_map(10, 10);
-    draw_map(x, y);
-    x = 250;
-    y = 250;
-    put_square(x, y, 60, 0x55aadd);
-    int cord[2];
-    cord[0] = x;
-    cord[1] = y;
-    mlx_key_hook(win_ptr, move, cord);
+	win_ptr = mlx_new_window(mlx_ptr, wx, wy, "mlx 1337");
+    init_pl_map(&player);
+    draw_map(&player);
+    put_character(player);
+    mlx_key_hook(win_ptr, move, &player);
+    // mlx_loop_hook(mlx_ptr, move, &player);
 	mlx_loop(mlx_ptr);
     return(0);
 }
--- cub3d/test.c
+++ cub3d/test.c
@@ -1,32 +1,32 @@
 #include <mlx.h>
+#include <math.h>
 #include "libft/libft.h"
 #include <unistd.h>
-
+#include <stdio.h>
 int	deal_key(int key, void *ptr)
 {
-	ft_putnbr_fd(key, 1);
-	ft_putchar_fd('\n', 1);
+	printf("%d\n", key);
+	// ft_putnbr_fd(key, 1);
+	// ft_putchar_fd('\n', 1);
 	return (0);
 }
 
-void drawcircle(void *mlx_ptr, void *win_ptr, int xc, int yc, int x, int y)
-{
-	mlx_pixel_put(mlx_ptr, win_ptr, xc+x, yc+y, 0xff0000);
-	mlx_pixel_put(mlx_ptr, win_ptr, xc-x, yc+y, 0xff0000);
-	mlx_pixel_put(mlx_ptr, win_ptr, xc+x, yc-y, 0xff0000);
-	mlx_pixel_put(mlx_ptr, win_ptr, xc-x, yc-y, 0xff0000);
-	mlx_pixel_put(mlx_ptr, win_ptr, xc+y, yc+x, 0xff0000);
-	mlx_pixel_put(mlx_ptr, win_ptr, xc-y, yc+x, 0xff0000);
-	mlx_pixel_put(mlx_ptr, win_ptr, xc+y, yc-x, 0xff0000);
-	mlx_pixel_put(mlx_ptr, win_ptr, xc-y, yc-x, 0xff0000);
-}
+// void drawcircle(void *mlx_ptr, void *win_ptr, int xc, int yc, int x, int y)
+// {
+// 	mlx_pixel_put(mlx_ptr, win_ptr, xc+x, yc+y, 0xff0000);
+// 	mlx_pixel_put(mlx_ptr, win_ptr, xc-x, yc+y, 0xff0000);
+// 	mlx_pixel_put(mlx_ptr, win_ptr, xc+x, yc-y, 0xff0000);
+// 	mlx_pixel_put(mlx_ptr, win_ptr, xc-x, yc-y, 0xff0000);
+// 	mlx_pixel_put(mlx_ptr, win_ptr, xc+y, yc+x, 0xff0000);
+// 	mlx_pixel_put(mlx_ptr, win_ptr, xc-y, yc+x, 0xff0000);
+// 	mlx_pixel_put(mlx_ptr, win_ptr, xc+y, yc-x, 0xff0000);
+// 	mlx_pixel_put(mlx_ptr, win_ptr, xc-y, yc-x, 0xff0000);
+// }
 
 int main()
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	int x;
-	int y;
 
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 800, 800, "mlx 1337");
@@ -57,30 +57,59 @@ int main()
 	// }
 	
 	// draw circle
-	int xc;
-	int yc;
-	int r;
-	int d;
+	// int xc;
+	// int yc;
+	// int r;
+	// int d;
 
-	xc = 400;
-	yc = 100;
-	r = 80;
-	x = 0;
-	y = r;
-	d = r;
-	
-	drawcircle(mlx_ptr, win_ptr, xc, yc, x, y);
-	while (y >= x)
+	// xc = 400;
+	// yc = 100;
+	// r = 80;
+	// x = 0;
+	// y = r;
+	// d = r;
+	//ber... algo
+	// drawcircle(mlx_ptr, win_ptr, xc, yc, x, y);
+	// while (y >= x)
+	// {
+	// 	x++;
+	// 	if (d > 0)
+	// 	{
+	// 		y--;
+	// 		d = d + 4 * (x - y) + 10;
+	// 	}
+	// 	else
+	// 		d = d + 4 * x + 6;
+	// 	drawcircle(mlx_ptr, win_ptr, xc, yc, x, y);
+	// }
+
+	// draw circle 2 :
+	int x;
+	int y;
+	int r;
+	float pi;
+	float phi;
+	float val;
+	int color;
+
+
+	r = 200;
+	phi = 0;
+	pi = 3.14159265359;
+	val = pi / 180;
+	color = 0xffffff;
+	while (r)
 	{
-		x++;
-		if (d > 0)
+		phi = 0;
+		while (phi <= 360)
 		{
-			y--;
-			d = d + 4 * (x - y) + 10;
+			x = 400 + (r * sin(phi * val));
+			y = 400 + (r * cos(phi * val));
+			mlx_pixel_put(mlx_ptr, win_ptr, x, y, color);
+			phi += 0.1;
+			color += 12;
 		}
-		else
-			d = d + 4 * x + 6;
-		drawcircle(mlx_ptr, win_ptr, xc, yc, x, y);
+		r--;
 	}
 	mlx_loop(mlx_ptr);
 }
