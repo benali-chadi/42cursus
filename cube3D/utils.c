07f00963f463a804361be7f2e9c832a2815dad89
Chadi Benali oulhaj <cbenali-@e1r6p12.1337.ma>
2020-02-23 22:30:08 +0100

    lk

--- cube3D/utils.c
+++ cube3D/utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbenali- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/23 22:27:21 by cbenali-          #+#    #+#             */
+/*   Updated: 2020/02/23 22:27:23 by cbenali-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cube3d.h"
 
 int		all_n(char *s)
@@ -9,10 +21,8 @@ int		all_n(char *s)
 		return (0);
 	s = ft_strtrim(s, " \t");
 	while (s[++i])
-	{
 		if (s[i] < 48 || s[i] > 57)
 			return (0);
-	}
 	return (1);
 }
 
@@ -27,7 +37,7 @@ void	check_comma(char *line)
 		{
 			coma++;
 			if (coma > 2)
-				ft_exit("Eroor\nToo many commas\n");
+				ft_exit("Eroor\nUse only two commas\n");
 		}
 		line++;
 	}
